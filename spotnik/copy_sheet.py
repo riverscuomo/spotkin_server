121351c28d10a87a86d8529fb42e3ccfd3aabedc
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2023-03-04 14:31:54 -0800

    Update copy_sheet.py

--- spotnik/copy_sheet.py
+++ spotnik/copy_sheet.py
@@ -2,14 +2,14 @@ import argparse
 import pygsheets  # pip3 install pygsheets
 
 
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1z5MejG6EKg8rf8vYKeFhw9XT_3PxkDFOrPSEKT_jYqI/edit#gid=1936655481"
+TEMPLATE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1z5MejG6EKg8rf8vYKeFhw9XT_3PxkDFOrPSEKT_jYqI/edit#gid=1936655481"
 SHEET_TITLE = "Spotify Controller"
 
 
 def copy_sheet(service_file, gmail):
 	gc = pygsheets.authorize(service_file=service_file)
 
-	template_sh = gc.open_by_url(SHEET_URL)
+	template_sh = gc.open_by_url(TEMPLATE_SHEET_URL)
 	copied_sh = gc.create(title=SHEET_TITLE, template=template_sh)
 
 	# optionally remove "validation" worksheet
