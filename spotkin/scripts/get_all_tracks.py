7c9c2df112c8db7070a698ebcbbdfb6b65ba4aa9
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2024-08-22 09:14:43 -0700

    getalltracks in 10 threads

--- spotkin/scripts/get_all_tracks.py
+++ spotkin/scripts/get_all_tracks.py
@@ -6,51 +6,85 @@ except:
     from spotkin.scripts.utils import log
 
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+def get_playlist_tracks_wrapper(args):
+    spotify, playlist_id, quantity, playlist_name = args
+    return sample_playlist_tracks(spotify, playlist_id, quantity, name=playlist_name)
+
+
 def get_all_tracks(job, spotify):
     """
-    This function will get the tracks from the playlists the user has specified in data/add_list.csv
+    This function will get the tracks from the playlists the user has specified
+    using parallel processing to speed up execution.
     """
     target_playlist_name = job["name"]
     log(
-        f"Getting tracks from the playlists earmarked for the {target_playlist_name} playlist..."
-    )
+        f"Getting tracks from the playlists earmarked for the {target_playlist_name} playlist...")
+
     my_picks = []
+    tasks = []
 
+    # Prepare tasks for parallel execution
     for row in job["recipe"]:
-        quantity = int(row["quantity"])
-
-        # try:
-        #     # How many songs you want to grab from the source playlist for this target playlist
-        #     quantity = int(row["quantity"])
-        # except KeyError:
-        #     log(
-        #         f"Error: '{target_playlist_name}' not found in the header row of add_list.csv"
-        #     )
-        #     exit()
-
+        quantity = int(row["quantity"]) if row["quantity"] else None
         if quantity is None or quantity == "" or quantity < 1:
-            # log("skipping row...")
             continue
 
-        # quantity = int(quantity)
-
-        # parse the id of the playlist from whatever the user has entered in config.py
         playlist_id = row["source_playlist_id"]
-
-        # for logging purposes
         playlist_name = row["source_playlist_name"]
 
-        new_tracks = sample_playlist_tracks(
-            spotify,
-            playlist_id,
-            quantity,
-            # skip_recents=skip_recents,
-            name=playlist_name,
-        )
+        tasks.append((spotify, playlist_id, quantity, playlist_name))
 
-        my_picks.extend(new_tracks)
+    # Use ThreadPoolExecutor for parallel execution
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_playlist = {executor.submit(
+            get_playlist_tracks_wrapper, task): task for task in tasks}
+
+        for future in as_completed(future_to_playlist):
+            task = future_to_playlist[future]
+            try:
+                new_tracks = future.result()
+                my_picks.extend(new_tracks)
+            except Exception as exc:
+                playlist_name = task[3]
+                log(f"Playlist {playlist_name} generated an exception: {exc}")
 
     return my_picks
+
+# def get_all_tracks(job, spotify):
+#     """
+#     This function will get the tracks from the playlists the user has specified
+#     """
+#     target_playlist_name = job["name"]
+#     log(
+#         f"Getting tracks from the playlists earmarked for the {target_playlist_name} playlist..."
+#     )
+#     my_picks = []
+
+#     for row in job["recipe"]:
+#         quantity = int(row["quantity"])
+
+#         if quantity is None or quantity == "" or quantity < 1:
+#             continue
+
+#         # parse the id of the playlist from whatever the user has entered in config.py
+#         playlist_id = row["source_playlist_id"]
+
+#         # for logging purposes
+#         playlist_name = row["source_playlist_name"]
+
+#         new_tracks = sample_playlist_tracks(
+#             spotify,
+#             playlist_id,
+#             quantity,
+#             name=playlist_name,
+#         )
+
+#         my_picks.extend(new_tracks)
+
+#     return my_picks
 
 
 # # Some playlist you want to repeat songs until
