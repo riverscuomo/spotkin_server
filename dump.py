e6aac110e68a303adde1f2bd47280e00641fa9bb
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2024-08-28 09:06:51 -0700

    file

---
+++ dump.py
@@ -0,0 +1,4 @@
+from rivertils import dump
+
+dump.dump(['.gitignore', '.\spotkin\data', 'CHANGELOG.md', 'LICENSE', '.git',
+          'spotkin.egg-info', '.\code-based.md', '.\dump.py', '.\.vscode', '.\spotkin\copy_sheet.py'])
