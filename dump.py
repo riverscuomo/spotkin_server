9b33d8985a8d8aceba715a5838365a7059076b4b
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2024-08-28 10:02:12 -0700

    upgrade rivertils

--- dump.py
+++ dump.py
@@ -1,4 +1,4 @@
 from rivertils import dump
 
-dump.dump(['.gitignore', '.\spotkin\data', 'CHANGELOG.md', 'LICENSE', '.git',
+dump.dump('spotkin_server', ['.gitignore', '.\spotkin\data', 'CHANGELOG.md', 'LICENSE', '.git',
           'spotkin.egg-info', '.\code-based.md', '.\dump.py', '.\.vscode', '.\spotkin\copy_sheet.py'])
